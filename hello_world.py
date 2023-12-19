3703cf282ab76da669a9983c12ba8e2d865f0533
Ilona Timonen <ilona.s.timonen@tuni.fi>
2023-12-19 13:44:56 +0200

    muutettu printtia

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,6 @@ def main():
     print("Hello world!")
 if __name__ == "__main__":
     main()
-print("Hello once again")
-print("Hello world!")
+print("Hello darkness my old friend")
 print("I've come to talk with you again")
 
